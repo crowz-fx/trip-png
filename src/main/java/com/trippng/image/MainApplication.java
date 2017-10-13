e2dd3e37c071e75e9c50699a8996413d271d6a70
lui <lui@crowz-fx>
2017-10-13 18:33:01 +0100

    the show is about to start

---
+++ src/main/java/com/trippng/image/MainApplication.java
@@ -0,0 +1,14 @@
+package com.trippng.image;
+
+/**
+ * Main entry point into the image processing framework.
+ * @author Lui Crowie
+ * @author James Jarrard
+ */
+public class MainApplication {
+
+	public static void main(String[] args) {
+		System.out.println("fast as fuq boiiii!");
+	}
+
+}
